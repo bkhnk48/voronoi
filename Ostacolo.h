8088e4dd746112b4bb59cae0d3b55166584a807e
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-05-11 05:35:35 +0700

    use Point/Distance instead of Punto/Distanze

--- Ostacolo.h
+++ Ostacolo.h
@@ -1,33 +1,33 @@
 /* Created by Simone Tinella, University of Catania*/
 
-#include "Punto.h"
+#include "Point.h"
 #include <math.h>
 #include <vector>
 
 class Ostacolo{
 	private:
-		std::vector<Punto*> vertices;
-		std::vector<Punto*> edges;
+		std::vector<Point*> vertices;
+		std::vector<Point*> edges;
 	public:
-		Ostacolo(std::vector<Punto*> *vert);
-		std::vector<Punto*> *getEdges();
-		static double Distanza(Punto p1,Punto p2);		
+		Ostacolo(std::vector<Point*> *vert);
+		std::vector<Point*> *getEdges();
+		static double Distance(Point p1,Point p2);		
 };
 
-Ostacolo::Ostacolo(std::vector<Punto*> *vert){
+Ostacolo::Ostacolo(std::vector<Point*> *vert){
 	vertices=*vert;
 	
-	if(vertices.size()>1){
-		for(int i=0;i < vertices.size();i++){
-			Punto *p1= vertices.at(i);
-            Punto *p2= vertices.at((i+1)%vertices.size());
+	if(vertices.size() > 1){
+		for(int i = 0;i < vertices.size(); i++){
+			Point *p1= vertices.at(i);
+            Point *p2= vertices.at((i+1) % vertices.size());
             
             double p1x= p1->getX();
             double p2x=p2->getX();
             double p1y=p1->getY();
             double p2y= p2->getY();
 			
-			Punto *tmp=p1;
+			Point *tmp=p1;
 			double tmp_x= tmp->getX();
 			double tmp_y= tmp->getY();
 			
@@ -35,28 +35,28 @@ Ostacolo::Ostacolo(std::vector<Punto*> *vert){
 				
 				edges.push_back(tmp);
 				
-				if(p1x==p2x && p1y!=p2y){ //retta verticale
-                    if(p1y<p2y)
+				if(p1x == p2x && p1y != p2y){ //retta verticale
+                    if(p1y < p2y)
                         tmp_y++;
-                    else if(p1y>p2y)
+                    else if(p1y > p2y)
                         tmp_y--;                   
-                }else if(p1x!=p2x && p1y==p2y){ //retta orizzontale
-                    if(p1x<p2x)
+                }else if(p1x != p2x && p1y == p2y){ //retta orizzontale
+                    if(p1x < p2x)
                         tmp_x++;
-                    else if(p1x>p2x)
+                    else if(p1x > p2x)
                         tmp_x--;
                 }else{ //retta obbligua
-                   if(p1x<p2x)
+                   if(p1x < p2x)
                         tmp_x++;
-                    else if(p1x>p2x)
+                    else if(p1x > p2x)
                         tmp_x--;
                    
-                    tmp_y= ((tmp_x-p1x)/(p2x-p1x))*(p2y-p1y) +p1y ;
+                    tmp_y= ((tmp_x - p1x) / (p2x - p1x))*(p2y - p1y) + p1y ;
                 }
 				
-				tmp= new Punto(tmp_x,tmp_y);
+				tmp= new Point(tmp_x,tmp_y);
 				
-			}while(!(tmp->getX()==p2->getX() && tmp->getY()==p2->getY()));
+			}while(!(tmp->getX() == p2->getX() && tmp->getY() == p2->getY()));
 			
 		}
 	}else edges.push_back(vertices.at(0));
@@ -64,11 +64,11 @@ Ostacolo::Ostacolo(std::vector<Punto*> *vert){
 }
 
 
-std::vector<Punto*> *Ostacolo::getEdges(){
+std::vector<Point*> *Ostacolo::getEdges(){
 	return &edges;
 }
 
-double Ostacolo::Distanza(Punto p1,Punto p2){
+double Ostacolo::Distance(Point p1,Point p2){
 	return sqrt((p1.getX()-p2.getX())*(p1.getX()-p2.getX()) + (p1.getY()-p2.getY())*(p1.getY()-p2.getY()));
 }
 
